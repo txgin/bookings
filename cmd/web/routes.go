a91917af4764c2137974011bb31c9767bf130d89
txgin <121838101+txgin@users.noreply.github.com>
2023-05-15 10:20:07 +0900

    Initial commit

---
+++ cmd/web/routes.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/txgin/bookings/pkg/config"
+	"github.com/txgin/bookings/pkg/handlers"
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+)
+
+func routes(app *config.AppConfig) http.Handler {
+
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
+	mux.Use(SessionLoad)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+
+	return mux
+}
